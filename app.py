da3a7e814c6c6b811da78eb28e41abe9a33e2cea
halilozercan <halilozercan@gmail.com>
2015-08-22 09:52:18 -0400

    git update

--- app.py
+++ app.py
@@ -2,9 +2,18 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+@app.route('/gitupdate')
+def gitupdate():
+	import subprocess
+	cmd = ["git","pull"]
+	p = subprocess.Popen(cmd, stdout = subprocess.PIPE,stderr=subprocess.PIPE,stdin=subprocess.PIPE)
+	out,err = p.communicate()
+	return out
+
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://root:43152957@localhost/ggkp"
 db = SQLAlchemy(app)
 
 if __name__ == '__main__':
-	app.run(host='127.0.0.1',port=4457,debug=True)
+	app.run(host='0.0.0.0',port=4457,debug=True)
 
