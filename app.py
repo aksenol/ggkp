6879e3237ec7ae10ec53c83ff190ec698a06e52b
Ahmet Kerim <aksenol@gmail.com>
2015-08-22 17:34:10 +0300

    Trying to initialize sqlalchemy

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from db.shared import db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://root:43152957@localhost/ggkp"
+
 
 @app.route('/gitupdate')
 def gitupdate():
@@ -11,8 +14,22 @@ def gitupdate():
 	out,err = p.communicate()
 	return out
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://root:43152957@localhost/ggkp"
-db = SQLAlchemy(app)
+@app.route('/dbreset')
+def dbreset():
+	db.init_app(app)
+	from db.user import User
+	from db.user import Group
+	db.drop_all()
+	db.create_all()
+	admin = User('admin', 'admin@example.com')
+	guest = Group('guest', 'guest@example.com')
+	db.session.add(admin)
+	db.session.add(guest)
+	db.session.commit()
+	return User.query.all()
+
+
+db.init_app(app)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',port=4457,debug=True)
--- reset_db.py
+++
@@ -1,8 +0,0 @@
-# to be run when database structure changes
-from app import db
-import db.user
-db.drop_all()
-db.create_all()
-# something like this
-
-
