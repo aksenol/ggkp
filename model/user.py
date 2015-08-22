e631193858e443327bc177923ab3074ddc49374c
Ahmet Kerim <aksenol@gmail.com>
2015-08-22 22:23:54 +0300

    new models

--- model/user.py
+++ model/user.py
@@ -2,9 +2,9 @@ from shared import db
 from subscribe import subscribe
 from ban import ban
 class User(db.Model):
-    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-   	picture = db.Column(db.String(120))
-   	fb_token = db.Column(db.String(120))
+	user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	picture = db.Column(db.String(120))
+	fb_token = db.Column(db.String(120))
 	first_name = db.Column(db.String(80))
 	last_name = db.Column(db.String(80))
 	email = db.Column(db.String(80))
@@ -19,8 +19,6 @@ class User(db.Model):
 		secondary = ban, backref = db.backref('banned_users', lazy = 'dynamic'))
 	admin_of = db.relationship('Group')
 
-	# i probably dont need the below line
-	#seen = db.relationship('Post', secondary=seen, backref=db.backref('seen_by', lazy='dynamic'))
     def __init__(self):
     	pass
 
